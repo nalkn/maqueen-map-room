34b6dc1cc7f467c402a46cad3314542f24ec829f
Nallet Killian <killian.nallet95@gmail.com>
2025-05-28 01:36:33 +0200

    First upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Maqueen-Map-Room
+Maqueen Map Room is a project for map a room with the 
---
+++ src/robot/main.py
@@ -0,0 +1,14 @@
+#---------------------------------------------------------------------------------
+# -*- coding: utf-8 -*-
+# MicroPython v1.18 on 2023-10-30; micro:bit v2.1.2 with nRF52833
+# Author: Killian Nallet
+# Date: 27/05/2025
+#---------------------------------------------------------------------------------
+
+# imports
+from microbit import *
+import radio
+
+# savoir faire un quart de tour
+
+# savoir faire un scan
